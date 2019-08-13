49fe85ab88583413547914f22f6c0bd13536a4db
mezz <mezzanine510@gmail.com>
2019-08-13 14:20:56 -0700

    Added basic laser partile system; refactored member variables

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,38 +8,36 @@ public class Player : MonoBehaviour
     [Tooltip("xSpeed")] [SerializeField] float xSpeed = 55f;
     [Tooltip("ySpeed")] [SerializeField] float ySpeed = 40f;
 
-    [SerializeField] float positionYawFactor = 0.25f;
+    [SerializeField] float positionYawFactor = 1.5f;
     [SerializeField] float controlYawFactor = 15f;
-    [SerializeField] float positionPitchFactor = 0.25f;
+    [SerializeField] float positionPitchFactor = 0f;
     [SerializeField] float controlPitchFactor = -20f;
     [SerializeField] float controlRollFactor = -20f;
+    float minXPos = -22f;
+    float maxXPos = 22f;
+    float minYPos = -14f;
+    float maxYPos = 14f;
 
-    float minXPos = -29f;
-    float maxXPos = 29f;
-    float minYPos = -15f;
-    float maxYPos = 17f;
     float xThrow;
     float yThrow;
-    float xOffsetThisFrame;
-    float yOffsetThisFrame;
     float delta;
-    Vector3 newLocalPosition;
 
     void Awake()
     {
 
     }
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         delta = Time.deltaTime;
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         Translate();
         Rotate();
         // float newXPos = currentLocalPos.x + xOffsetThisFrame;
@@ -69,13 +67,13 @@ public class Player : MonoBehaviour
 
     void Translate()
     {
-        newLocalPosition = transform.localPosition;
+        Vector3 newLocalPosition = transform.localPosition;
 
-        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        // xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        // yThrow = CrossPlatformInputManager.GetAxis("Vertical");
 
-        xOffsetThisFrame = xThrow * xSpeed * delta;
-        yOffsetThisFrame = yThrow * ySpeed * delta;
+        float xOffsetThisFrame = xThrow * xSpeed * delta;
+        float yOffsetThisFrame = yThrow * ySpeed * delta;
 
         newLocalPosition.x = Mathf.Clamp(transform.localPosition.x + xOffsetThisFrame, minXPos, maxXPos);
         newLocalPosition.y = Mathf.Clamp(transform.localPosition.y + yOffsetThisFrame, minYPos, maxYPos);
