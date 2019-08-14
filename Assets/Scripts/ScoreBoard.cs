677413ed1479273fb565c69f28040b1d377fd519
Mezz <mezzanine510@gmail.com>
2019-08-14 14:34:16 -0700

    Modified scoreboard styling; created ScoreBoard.cs

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 14;
+    
+    int score;
+    Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    public void ScoreHit()
+    {
+        score = score + scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
