ece558fe6664fc19235e9851536603c8dacfe9a8
Mezz <mezzanine510@gmail.com>
2019-08-14 14:06:55 -0700

    Created enemy explosion instantiation on enemy death; created UI base

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject enemyDeathFX;
+    [SerializeField] GameObject parent;
+
+    private void Awake() {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
     private void OnParticleCollision(GameObject other) {
+        GameObject fx = Instantiate(enemyDeathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent.transform;
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField] float delay = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+
+    void Update()
+    {
+        
+    }
+}
